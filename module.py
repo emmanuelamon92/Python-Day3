73dba47cf0a9ba1beca916917c10c81ed0b2b750
Emmanuel Amon <emmanuelamon92@gmail.com>
2021-02-11 10:44:33 -0500

    uncommented code

--- module.py
+++ module.py
@@ -1,12 +1,12 @@
-# length = int(input("Enter length: "))
-# width = int(input("Enter width: "))
+length = int(input("Enter length: "))
+width = int(input("Enter width: "))
 def house(length, width):
     area = length * width
     print(f'The area of the house is {area}.')
     return area
 house(length, width)
 
-# radius = int(input('Enter radius: '))
+radius = int(input('Enter radius: '))
 def circumference(radius):
     pi = 3.14
     circumference = 2 * pi * radius
