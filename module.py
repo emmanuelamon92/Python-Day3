ed5c733dd9883238196206b2ec1a602ae889aece
Emmanuel Amon <emmanuelamon92@gmail.com>
2021-02-11 12:05:58 -0500

    bug fixes with calling module to notebook

--- module.py
+++ module.py
@@ -1,16 +1,17 @@
-length = int(input("Enter length: "))
-width = int(input("Enter width: "))
+import math
+# length = int(input("Enter length: "))
+# width = int(input("Enter width: "))
 def house(length, width):
     area = length * width
     print(f'The area of the house is {area}.')
     return area
-house(length, width)
+# house(length, width)
 
-radius = int(input('Enter radius: '))
+# radius = int(input('Enter radius: '))
 def circumference(radius):
     pi = 3.14
     circumference = 2 * pi * radius
-    print (f'The circumference of the circle is {circumference}.')
+    # print (f'The circumference of the circle is {circumference}.')
     return circumference
 
-circumference(radius)
+# circumference(radius)
